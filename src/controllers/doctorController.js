63a6c9808f9a6018ae93f1199c373571bb703359
Parvez Saifi <saifip621@gmail.com>
2026-07-13 18:10:02 +0530

    Refactor doctor grouping and add filtering logic

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -4,17 +4,14 @@ function groupByDepartment(doctors) {
     const grouped = {};
 
     doctors.forEach((doctor) => {
-        const department = doctor.department;
-
-        if (!grouped[department]) {
-            grouped[department] = [];
+        if (!grouped[doctor.department]) {
+            grouped[doctor.department] = [];
         }
 
-        grouped[department].push({
+        grouped[doctor.department].push({
             doctor_id: doctor._id,
-            doctor_name: doctor.doctor_name,
+            name: doctor.name,
             is_available_today: doctor.is_available_today,
-            slotsAvailable: doctor.slotsAvailable,
         });
     });
 
@@ -23,16 +20,29 @@ function groupByDepartment(doctors) {
 
 exports.getDoctors = async (req, res) => {
     try {
-        const doctors = await Doctor.find();
+        console.log("Query:", req.query);
+
+        const filter = {};
+
+        if (req.query.department) {
+            filter.department = req.query.department;
+        }
+
+        if (req.query.available !== undefined) {
+            filter.is_available_today = req.query.available === "true";
+        }
+
+        console.log("Filter:", filter);
+
+        const doctors = await Doctor.find(filter);
+
+        console.log("Doctors found:", doctors.length);
 
         const grouped = groupByDepartment(doctors);
 
         res.status(200).json(grouped);
     } catch (err) {
         console.error(err);
-
-        res.status(500).json({
-            error: err.message,
-        });
+        res.status(500).json({ error: err.message });
     }
 };
