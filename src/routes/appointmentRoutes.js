fbf5c124547a159b796138e78c4d687c336fec7d
Parvez Saifi <saifip621@gmail.com>
2026-07-13 18:08:43 +0530

    Add new routes for appointment management

--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const appointmentController = require("../controllers/appointmentController");
 
+router.get("/dashboard", appointmentController.getDashboardAppointments);
+router.get("/lookup", appointmentController.lookupAppointment);
 router.get("/", appointmentController.getAppointments);
+router.post("/", appointmentController.createAppointment);
+router.patch("/:id/cancel", appointmentController.cancelAppointment);
+router.patch("/:id/done", appointmentController.markAppointmentDone);
 
 module.exports = router;
