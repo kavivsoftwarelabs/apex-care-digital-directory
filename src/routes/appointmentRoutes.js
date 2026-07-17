487d6189348c8108283796449940dd337923a630
Parvez Saifi <saifip621@gmail.com>
2026-07-17 18:52:26 +0530

    Refactor appointment routes and update handlers

--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -5,15 +5,9 @@ const appointmentController = require("../controllers/appointmentController");
 router.post("/", appointmentController.bookAppointment);
 router.post("/cancel", appointmentController.cancelAppointment);
 
-module.exports = router;
-
-const appointmentController = require("../controllers/appointmentController");
-
-router.get("/dashboard", appointmentController.getDashboardAppointments);
-router.get("/lookup", appointmentController.lookupAppointment);
 router.get("/", appointmentController.getAppointments);
-router.post("/", appointmentController.createAppointment);
-router.patch("/:id/cancel", appointmentController.cancelAppointment);
+router.get("/lookup", appointmentController.lookupAppointment);
+router.patch("/:id/cancel", appointmentController.cancelAppointmentById);
 router.patch("/:id/done", appointmentController.markAppointmentDone);
 
 module.exports = router;
